d1c204b8b023118562f2534d6d04f6bc5d1858a9
김도현 KimDohyun <163822282+pu2rile@users.noreply.github.com>
2026-05-05 17:22:05 +0900

    fix: 충돌 에러 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
-import MainPage from "./pages/MainPage";
-
-function App() {
-  return <MainPage />;
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainPage from "./pages/MainPage";
 import Login from "./pages/Login";
 import AuthCallback from "./pages/AuthCallback";
 
@@ -10,16 +7,20 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* 첫 화면(/) Login*/}
-        <Route path="/" element={<Login />} />
+        {/* 첫 화면(/) MainPage */}
+        <Route path="/" element={<MainPage />} />
 
-        {/* 게스트 로그인 후 경로*/}
+        {/* 로그인 페이지 */}
+        <Route path="/login" element={<Login />} />
+
+        {/* 게스트 로그인 후 경로 */}
         <Route path="/guest" element={<div> after guest login </div>} />
+
         {/* 카카오 인가 코드 수신 경로 */}
         <Route path="/auth/callback" element={<AuthCallback />} />
 
-         {/* 카카오 로그인 완료 후 경로 */}
-         <Route path="/main" element={<div> after kakao login </div>} />
+        {/* 카카오 로그인 완료 후 경로 */}
+        <Route path="/main" element={<MainPage />} />
 
       </Routes>
     </BrowserRouter>
